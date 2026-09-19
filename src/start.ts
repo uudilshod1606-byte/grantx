abaca6201d9039328eb633ec0ac91a1400f92cbc
uudilshod1606-byte <uudilshod1606@gmail.com>
2026-09-19 14:20:37 +0500

    Use TanStack Start CSRF middleware API

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,4 @@
-import { createStart, createMiddleware } from "@tanstack/react-start";
-import { createCsrfMiddleware } from "@tanstack/start-client-core";
+import { createStart, createMiddleware, createCsrfMiddleware } from "@tanstack/react-start";
 
 import { renderErrorPage } from "./lib/error-page";
 import { attachSupabaseAuth } from "@/integrations/supabase/auth-attacher";
